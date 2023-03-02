b19c8d0f8b2d09156e4b442259b748821d3692cf
Emma Plessinger <plessinger.em@gmail.com>
2023-03-01 17:35:55 -0700

    cleaned up the context file

--- Mission09_plessem/Models/Books.cs
+++ Mission09_plessem/Models/Books.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 // Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
 // If you have enabled NRTs for your project, then un-comment the following line:
@@ -9,14 +10,24 @@ namespace Mission09_plessem.Models
 {
     public partial class Book
     {
+        [Key]
+        [Required]
         public long BookId { get; set; }
+        [Required]
         public string Title { get; set; }
+        [Required]
         public string Author { get; set; }
+        [Required]
         public string Publisher { get; set; }
+        [Required]
         public string Isbn { get; set; }
+        [Required]
         public string Classification { get; set; }
+        [Required]
         public string Category { get; set; }
+        [Required]
         public long PageCount { get; set; }
+        [Required]
         public double Price { get; set; }
     }
 }
