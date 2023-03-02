23047503ed326177a2cf8cdb3c613661dff09952
Emma Plessinger <plessinger.em@gmail.com>
2023-03-01 17:33:02 -0700

    brining in db and website works

--- Mission09_plessem/Startup.cs
+++ Mission09_plessem/Startup.cs
@@ -1,8 +1,11 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Mission09_plessem.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,9 +17,23 @@ namespace Mission09_plessem
     {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        
+        public IConfiguration Configuration { get; set; }
+
+        public Startup (IConfiguration temp)
+        {
+            Configuration = temp;
+        }
+
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddDbContext<BookstoreContext>(options =>
+           {
+               options.UseSqlite(Configuration["ConnectionStrings:BookstoreDBConnection"]);
+
+           });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
