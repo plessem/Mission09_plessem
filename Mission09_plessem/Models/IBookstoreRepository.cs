13f7df09e95e6fc757d8749d431803b44eba130a
Emma Plessinger <plessinger.em@gmail.com>
2023-03-01 18:06:55 -0700

    repo style works

---
+++ Mission09_plessem/Models/IBookstoreRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission09_plessem.Models
+{
+    //interface -- not a class but a template for a class 
+    public interface IBookstoreRepository
+    {
+        IQueryable<Book> Books { get; }
+    }
+}
--- Mission09_plessem/Startup.cs
+++ Mission09_plessem/Startup.cs
@@ -34,6 +34,8 @@ namespace Mission09_plessem
                options.UseSqlite(Configuration["ConnectionStrings:BookstoreDBConnection"]);
 
            });
+
+            services.AddScoped<IBookstoreRepository, EFBookstoreRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
