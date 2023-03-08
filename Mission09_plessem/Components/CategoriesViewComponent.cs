301e24900212bdeed3477a7b29e2a8c01b8953e5
Emma Plessinger <plessinger.em@gmail.com>
2023-03-07 21:14:30 -0700

    categories listing, not linked

---
+++ Mission09_plessem/Components/CategoriesViewComponent.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Mission09_plessem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission09_plessem.Components
+{
+    public class CategoriesViewComponent : ViewComponent
+    {
+        private IBookstoreRepository repo { get; set; }
+
+        public CategoriesViewComponent (IBookstoreRepository temp)
+        {
+            repo = temp;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            //select category types
+            var categories = repo.Books
+                .Select(x => x.Category)
+                .Distinct()
+                .OrderBy(x => x); //default order
+            return View(categories);
+        }
+    }
+}
