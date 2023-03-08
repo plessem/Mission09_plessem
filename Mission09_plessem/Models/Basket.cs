6244cdb8a8c3e695f08558f02cf466ea5489f8e9
Emma Plessinger <plessinger.em@gmail.com>
2023-03-07 22:27:42 -0700

    page model

---
+++ Mission09_plessem/Models/Basket.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission09_plessem.Models
+{
+    public class Basket
+    {
+        public List<BasketLineItem> Items { get; set; } = new List<BasketLineItem>();
+
+        public void AddItem (Book bk, int qty)
+        {
+            BasketLineItem line = Items
+                .Where(b => b.Book.BookId == bk.BookId)
+                .FirstOrDefault();
+
+            if (line == null)
+            {
+                Items.Add(new BasketLineItem
+                {
+                    Book = bk,
+                    Quantity = qty
+                });
+            }
+            else
+            {
+                line.Quantity += qty;
+            }
+        }
+
+        public double CalculateTotal()
+        {
+            double sum = Items.Sum(x => x.Quantity * x.Book.Price);
+            return sum;
+        }
+    }
+
+
+    //just one thing in the basket
+    public class BasketLineItem
+    {
+        public int LineID { get; set; }
+        public Book Book { get; set; }
+        public int Quantity { get; set; }
+    }
+}
--- Mission09_plessem/Pages/Checkout.cshtml.cs
+++ Mission09_plessem/Pages/Checkout.cshtml.cs
@@ -4,13 +4,32 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Mission09_plessem.Models;
 
 namespace Mission09_plessem.Pages
 {
     public class CheckoutModel : PageModel
     {
+        private IBookstoreRepository repo { get; set; }
+        public CheckoutModel (IBookstoreRepository temp)
+        {
+            repo = temp;
+        }
+
+        public Basket Basket { get; set; }
+
         public void OnGet()
         {
+        }
+
+        public IActionResult OnPost(int bookId)
+        {
+            Book b = repo.Books.FirstOrDefault(x => x.BookId == bookId);
+
+            Basket = new Basket();
+
+            Basket.AddItem(b, 1);
+            return RedirectToPage();
         }
     }
 }
