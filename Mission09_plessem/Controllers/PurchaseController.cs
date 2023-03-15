04170995e97c8f7ebdab4e2286803f26a6b1f2d2
Emma Plessinger <plessinger.em@gmail.com>
2023-03-14 21:59:58 -0600

    form shows up but book list on home page gone

---
+++ Mission09_plessem/Controllers/PurchaseController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Mission09_plessem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission09_plessem.Controllers
+{
+    public class PurchaseController : Controller
+    {
+        public IActionResult Checkouts()
+        {
+            return View(new Purchase());
+        }
+    }
+}
