7381ae07623db58440aa70726a6f59028e52d6f3
Emma Plessinger <plessinger.em@gmail.com>
2023-03-15 18:39:20 -0600

    cart summary works

---
+++ Mission09_plessem/Components/CartSummaryViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Mission09_plessem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission09_plessem.Components
+{
+    public class CartSummaryViewComponent : ViewComponent
+    {
+        private Basket cart;
+        public CartSummaryViewComponent(Basket cartService)
+        {
+            cart = cartService;
+        }
+        public IViewComponentResult Invoke()
+        {
+            return View(cart);
+        }
+    }
+}
